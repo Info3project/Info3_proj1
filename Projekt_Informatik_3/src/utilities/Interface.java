938a359344ee9bb14f96ab1eabaa731f7d59ea4a
PhilippKopp <philipp@koppmaps.de>
2014-11-26 08:45:41 +0100

    Klassen zum Speichern hinzugefügt

---
+++ Projekt_Informatik_3/src/utilities/Interface.java
@@ -0,0 +1,8 @@
+package utilities;
+
+import java.util.Properties;
+
+public interface Interface {
+	public Properties laden(String verzeichnis);
+	public void speichern(Properties daten, String verzeichnis);
+}
