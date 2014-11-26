af38903cc81e9c0da40e2f4b69d3c5a6227b57ed
Uch21 <benni@Bennis-Air.fh-reutlingen.de>
2014-11-26 11:13:36 +0100

    first element

--- Projekt_Informatik_3/src/utilities/Zentralverwaltung.java
+++ Projekt_Informatik_3/src/utilities/Zentralverwaltung.java
@@ -2,74 +2,79 @@ package utilities;
 
 import java.util.Vector;
 import java.util.Iterator;
+
 import command.*;
 
 public abstract class Zentralverwaltung {
-	private Vector <Prototyp> prototypen = new Vector<Prototyp>();
-	private Vector <Command> programmablauf= new Vector <Command>();
+	private Vector<Prototyp> prototypen = new Vector<Prototyp>();
+	private Vector<Command> programmablauf = new Vector<Command>();
 
 	/**
 	 * Befaellt den ersten Vector mit allen Prototypobjekten
 	 *
 	 */
-	public void createPrototypen(){
+	public void createPrototypen() {
 		prototypen.addElement(new Prototyp("Direction"));
 		prototypen.addElement(new Prototyp("Gear"));
 		prototypen.addElement(new Prototyp("Goto"));
 		prototypen.addElement(new Prototyp("If"));
 		prototypen.addElement(new Prototyp("Assignment"));
-		
+
 	}
+
 	/**
-	 * Fuegt einen Schritt in den Programmablauf Vector ein
-	 * und erstellt diesen aus dem entsprechenden Prototypen
+	 * Fuegt einen Schritt in den Programmablauf Vector ein und erstellt diesen
+	 * aus dem entsprechenden Prototypen
+	 * 
 	 * @param prototyp
 	 */
-	public void addStep(Prototyp prototyp){
+	public void addStep(Prototyp prototyp) {
 		programmablauf.addElement(prototyp.createInstance());
 	}
-	
+
 	/**
 	 * entfernt den entsprechenden Befehl aus dem Programmablauf
+	 * 
 	 * @param iD
 	 */
-	public void removeStep(int iD){
-		Iterator <Command> it = programmablauf.iterator();
-		while(it.hasNext()){
-			if (it.next().getStepID()==iD){
+	public void removeStep(int iD) {
+		Iterator<Command> it = programmablauf.iterator();
+		while (it.hasNext()) {
+			if (it.next().getId() == iD) {
 				it.remove();
 			}
 		}
 	}
-	
+
 	/**
 	 * schiebt den Befehl im Programmablauf einen Schritt nach vorne
+	 * 
 	 * @param iD
 	 */
-	public void incOrder(int iD){
-		Iterator <Command> it = programmablauf.iterator();
-		int index=0;
-		while(it.hasNext()){
-			if (it.next().getStepID()==iD){
-				Command temp = programmablauf.get(index-1);
-				programmablauf.set(index-1, programmablauf.get(index));
-				programmablauf.set(index, temp);
+	public void incOrder(int iD) {
+		Iterator<Command> it = programmablauf.iterator();
+		int index = 0;
+		if (it.next().getId() == iD) {
+		} else {
+			while (it.hasNext()) {
+				if (it.next().getId() == iD) {
+					Command temp = programmablauf.get(index - 1);
+					programmablauf.set(index - 1, programmablauf.get(index));
+					programmablauf.set(index, temp);
+				}
+
+				index++;
 			}
-			
-			index++;
 		}
 	}
-	
-	
+
 	/**
 	 * schiebt den Befehl im Programmablauf einen Schritt nach hinten
+	 * 
 	 * @param iD
 	 */
-	public void decOrder (int iD){
-		
+	public void decOrder(int iD) {
+
 	}
 
 }
-
-
-
