3bceaa47f054d199169dab9c5f3792297ee970bc
Uch21 <benni@Bennis-MacBook-Air.local>
2014-12-03 10:40:35 +0100

    BUTTONS

--- Projekt_Informatik_3/src/view/VerwaltungView.java
+++ Projekt_Informatik_3/src/view/VerwaltungView.java
@@ -19,6 +19,12 @@ public class VerwaltungView extends JFrame {
 	public VerwaltungView() {
 		super("Verwaltung");
 		setLayout(new GridLayout(10, 10));
+		JButton b1 = new JButton(("add"));
+		JButton b2 = new JButton(("remove"));
+		b1.addActionListener(null);
+		b2.addActionListener(null);
+		add(b1);
+		add(b2);
 	}
 
 	public static void main(String[] args) {
