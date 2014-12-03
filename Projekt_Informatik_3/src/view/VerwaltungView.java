f3b5039da5272aca15d57e1a5ac6af2351233478
Uch21 <benni@Bennis-MacBook-Air.local>
2014-12-03 10:29:03 +0100

    view hinzugefügt

---
+++ Projekt_Informatik_3/src/view/VerwaltungView.java
@@ -0,0 +1,21 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+
+/**
+ * @author Haid/Kopp
+ *
+ */
+public class VerwaltungView extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VerwaltungView() {
+		super("Verwaltung");
+		setLayout(new GridLayout(10,10));
+	}
+}
